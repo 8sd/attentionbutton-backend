b89b17e9f90be93a98cb40e37a60435657a914f4
8sd <github@sebdu.de>
2018-08-16 16:40:39 +0200

    Implemented mangement for lock to differenciate between IDs of alerts

---
+++ Controllers/LockManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace attentionbutton_backend.Controllers {
+    public class LockManager {
+        private Dictionary<String, Lock> dic = new Dictionary<String, Lock>();
+        public Lock getLock (String id){
+            if(dic.ContainsKey(id)){
+                return dic[id];
+            } else {
+                Lock tempLock = new Lock();
+                dic.Add(id, tempLock);
+                return dic [id];
+            }
+        }
+    }
+}
--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,11 +13,12 @@ namespace attentionbutton_backend.Controllers {
 
     [Route("/"), ApiController]
     public class ValuesController : ControllerBase {
-        private static Lock waitHandler = new Lock();
+        private static LockManager lockManager = new LockManager();
 
         [HttpGet("{id}")]
         public ActionResult<String> waitForAlert(String id) {
-            if (waitHandler.wait(100)) {
+            Lock waitHandler = lockManager.getLock (id);
+            if (waitHandler.wait()) {
                 return "alert";
             } else {
                 return "no alert " + id;
@@ -26,6 +27,7 @@ namespace attentionbutton_backend.Controllers {
         
         [HttpPost("{id}")]
         public ActionResult<String> triggerAlert(String id) {
+            Lock waitHandler = lockManager.getLock (id);
             waitHandler.unwaitall();
             return "alert initiated " + id;
         }
