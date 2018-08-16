6f6c0352648a905c1125728607917404c8013628
8sd <github@sebdu.de>
2018-08-16 16:31:55 +0200

    Added access to ID parameter of calls

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace attentionbutton_backend.Controllers {
+    public class ID {
+        [JsonProperty("ID")]
+        public String id {get;set;}
+    }
+
     [Route("/"), ApiController]
     public class ValuesController : ControllerBase {
         private static Lock waitHandler = new Lock();
 
-        [HttpGet]
-        public ActionResult<String> Get() {
-            if (waitHandler.wait()) {
+        [HttpGet("{id}")]
+        public ActionResult<String> waitForAlert(String id) {
+            if (waitHandler.wait(100)) {
                 return "alert";
             } else {
-                return "no alert";
+                return "no alert " + id;
             }
         }
         
-        [HttpPost]
-        public ActionResult<String> HttpPost() {
+        [HttpPost("{id}")]
+        public ActionResult<String> triggerAlert(String id) {
             waitHandler.unwaitall();
-            return "alert initiated";
+            return "alert initiated " + id;
         }
     }
 }
